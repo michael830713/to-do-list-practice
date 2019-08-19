9fa6147437cca8c3641f1d05ff0c999e20e6600a
Mike <michael830713@gmail.com>
2019-08-19 15:16:54 +0800

    clean up some code

--- lib/edit.dart
+++ lib/edit.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class EditPage extends StatelessWidget {
+class EditPage extends StatefulWidget {
 //  @override
 //  _EditPageState createState() => _EditPageState();
   String todos;
@@ -8,17 +8,35 @@ class EditPage extends StatelessWidget {
   EditPage({Key key, @required this.todos}) : super(key: key);
 
   @override
+  _EditPageState createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
+  var myController;
+
+  @override
+  void initState() {
+//    myController.addListener(_printLatestValue);
+  }
+
+  _printLatestValue() {
+    widget.todos = '${myController.text}';
+  }
+
+  @override
   Widget build(BuildContext context) {
     Future<bool> _onWillPop() {
-      Navigator.pop(context,'this is it');
+      Navigator.pop(context, myController.text);
       return new Future.value(false);
     }
+
+//    myController.addListener(_printLatestValue);
     // TODO: implement build
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
         appBar: AppBar(
-          title: Text(' To-Do $todos'),
+          title: Text(' To-Do ${widget.todos}'),
         ),
         body: Column(
           children: <Widget>[
@@ -27,35 +45,35 @@ class EditPage extends StatelessWidget {
               child: Container(
                 height: 200,
                 child: TextFormField(
-                  initialValue: todos,
+//                  initialValue: todos,
+//                  onEditingComplete: todos = myController.text,
+                  controller: myController =
+                      TextEditingController(text: widget.todos),
                 ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RaisedButton(
-                  color: Colors.grey,
-//                color: Colors.red,
-                  onPressed: () {},
-                  child: Text('捨棄'),
-                ),
-                RaisedButton(
-                  color: Colors.blue,
-                  onPressed: () {},
-                  child: Text('確認'),
-                )
-              ],
-            )
+//            Row(
+//              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//              children: <Widget>[
+//                RaisedButton(
+//                  color: Colors.grey,
+////                color: Colors.red,
+//                  onPressed: () {},
+//                  child: Text('捨棄'),
+//                ),
+//                RaisedButton(
+//                  color: Colors.blue,
+//                  onPressed: () {},
+//                  child: Text('確認'),
+//                )
+//              ],
+//            )
           ],
         ),
+
       ),
-
     );
-
   }
-
-
 }
 
 //class _EditPageState extends State<EditPage> {
